3014bed5a85a449e207adfba5e4ca5bf64bd0059
Nastya <kohasya@mail.ru>
2020-06-02 16:31:18 +0300

    Добавлен файл JS, создано модальное окно

--- src/js/main.js
+++ src/js/main.js
@@ -3,9 +3,13 @@ const modal = document.querySelector('#modal');
 const close = document.querySelector('#close');
 
 button.addEventListener('click', () => {
-    modal.classList.add('modal_active');  
+    modal.classList.add('modal_active');
+
+
 });
 
 close.addEventListener('click', () => {
     modal.classList.remove('modal_active')
+
 });
+
